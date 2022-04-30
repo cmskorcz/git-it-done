cb66eb3b386344ad7085de76304c38c7003086dc
cmskorcz <chaseskorey@icloud.com>
2022-04-29 20:28:45 -0400

    pushing to work on desktop

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+let userFormEl = document.getElementById('user-form');
+let nameInputEl = document.getElementById('username');
+
 const getUserRepos = (user) => {
     let userApi = `https://api.github.com/users/${user}/repos`;
 
@@ -10,4 +13,10 @@ const getUserRepos = (user) => {
         });
 };
 
+const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(event);    
+}
+
 getUserRepos();
+userFormEl.addEventListener('submit', formSubmitHandler)
