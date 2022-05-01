f44bdb803915c37b5fa9da5bb350e2441da91595
Chase Skorczewski <chaseskorey@icloud.com>
2022-05-01 12:59:59 -0400

    display repos and open issues to page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 let userFormEl = document.getElementById('user-form');
 let nameInputEl = document.getElementById('username');
+let repoContainerEl = document.getElementById('repos-container');
+let repoSearchTerm = document.getElementById('repo-search-term');
 
 const getUserRepos = (user) => {
     let userApi = `https://api.github.com/users/${user}/repos`;
@@ -8,7 +10,7 @@ const getUserRepos = (user) => {
         .then((res) => {
             res.json()
                 .then((data) => {
-                    console.log(data);
+                    displayRepos(data, user);
                 })
         });
 };
@@ -23,7 +25,36 @@ const formSubmitHandler = (event) => {
     } else {
         alert('Please enter a GitHub username');
     }
+};
+
+const displayRepos = (repos, searchTerm) => {
+    repoContainerEl.textContent = '';
+    repoSearchTerm.textContent = searchTerm;
+
+    for (let i = 0; i < repos.length; i++) {
+        let repoName = `${repos[i].owner.login}/${repos[i].name}`;
+
+        let repoEl = document.createElement('div');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+
+        let titleEl = document.createElement('span');
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl);
+
+        let statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
+
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = `<i class="fas fa-times status-icon icon-danger"></i>${repos[i].open_issues_count} issue(s)`;
+        } else {
+            statusEl.innerHTML = `<i class="fas fa-check-square status-icon icon-success"></i>`;
+        }
+
+        repoEl.appendChild(statusEl);
+        repoContainerEl.appendChild(repoEl);
+    }
 }
 
 getUserRepos();
-userFormEl.addEventListener('submit', formSubmitHandler)
+userFormEl.addEventListener('submit', formSubmitHandler);
